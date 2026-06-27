d7f6bdb27aca32d202b1ab28ea47c7d09af30fa7
Carson Rivera <carsonrivera04@gmail.com>
2026-06-27 16:05:16 -0400

    Changed title

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
       <h1>Senior Year Max&trade;:</h1>
       <p>90,000 hours. 3,750 days. 123 months. 10.25 years.</p>
       <p>That is the average amount of time someone will spend at w*rk over a lifetime.</p>
-      <p>But before, we get there...</p>
+      <p>But before we get there...</p>
       <p>We will do everything (and I mean everything) there is to do at THE Florida State University.</p>
       <p>Welcome to Senior Year Max&trade;</p>
       <ComponentArea />
